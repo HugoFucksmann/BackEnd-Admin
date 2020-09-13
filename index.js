9b25cd047fcd5997b93ee3fba6aa420fb67a26e6
Hugo Fucksmann <hugoffuksmann@gmail.com>
2020-09-13 11:42:39 -0300

    SPA control

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path');
 
 //kaDTrq6BPiKMHlMt
 //mean_user
@@ -36,6 +37,12 @@ app.use( '/api/medicos', require('./routes/medicos') );
 app.use( '/api/todo', require('./routes/busqueda') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/upload', require('./routes/uploads') );
+
+//ultimo para desplegar
+app.get("*"),
+  (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public/index.html"));
+  };
 
 app.listen( process.env.PORT, () => {
     console.log( 'server iniciado en puerto ', process.env.PORT );
